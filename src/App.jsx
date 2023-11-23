6777098fdaff1109f9f6ebaa632fcf0abc359d57
Pierre Arnaudet <pierre.arnaudet44@gmail.com>
2023-11-23 12:23:20 +0100

    init structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,40 @@
+import "./App.css";
+import logo from "./assets/logo.png";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="presents">
+        <div className="present">1</div>
+        <div className="present">2</div>
+        <div className="present">3</div>
+        <div className="present">4</div>
+        <div className="present">5</div>
+        <div className="present">6</div>
+        <div className="present">7</div>
+        <div className="present">8</div>
+        <div className="present">9</div>
+        <div className="present">10</div>
+        <div className="present">11</div>
+        <div className="present">12</div>
+        <div className="present">13</div>
+        <div className="present">14</div>
+        <div className="present">15</div>
+        <div className="present">16</div>
+        <div className="present">17</div>
+        <div className="present">18</div>
+        <div className="present">19</div>
+        <div className="present">20</div>
+        <div className="present">21</div>
+        <div className="present">22</div>
+        <div className="present">23</div>
+        <div className="present">24</div>
+      </div>
+    </>
+  );
 }
 
 export default App;
