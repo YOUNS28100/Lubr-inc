06a3c373a41aecbfadbd8104f0fc89c5c4fc5879
Pierre Arnaudet <pierre.arnaudet44@gmail.com>
2023-11-23 13:04:16 +0100

    map good

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import logo from "./assets/logo.png";
+import data from "./data.json";
 
 function App() {
   return (
@@ -8,30 +9,12 @@ function App() {
         <img src={logo} alt="logo" />
       </div>
       <div className="presents">
-        <div className="present">1</div>
-        <div className="present">2</div>
-        <div className="present">3</div>
-        <div className="present">4</div>
-        <div className="present">5</div>
-        <div className="present">6</div>
-        <div className="present">7</div>
-        <div className="present">8</div>
-        <div className="present">9</div>
-        <div className="present">10</div>
-        <div className="present">11</div>
-        <div className="present">12</div>
-        <div className="present">13</div>
-        <div className="present">14</div>
-        <div className="present">15</div>
-        <div className="present">16</div>
-        <div className="present">17</div>
-        <div className="present">18</div>
-        <div className="present">19</div>
-        <div className="present">20</div>
-        <div className="present">21</div>
-        <div className="present">22</div>
-        <div className="present">23</div>
-        <div className="present">24</div>
+        {data.map((l) => (
+          <div className="present" key={l.id}>
+            <img src={l.picture} alt={`lutin${l.id}`} />
+            {l.id}
+          </div>
+        ))}
       </div>
     </>
   );
